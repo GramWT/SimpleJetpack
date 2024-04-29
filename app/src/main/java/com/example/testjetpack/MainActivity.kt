94a1398e10b8a2dd9854e1d9a18fc13a6a53eef0
Woratum Toroek <gram1798@gmail.com>
2024-04-29 21:56:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testjetpack/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.testjetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testjetpack.ui.theme.TestJetpackTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            val painter = painterResource(id = R.drawable.naruto)
+//            val desc = "5555"
+//            val title = "Gram"
+//            
+//            Column {
+//                Box(modifier = Modifier
+//                    .fillMaxSize(0.5f)
+//                    .padding(16.dp)) {
+//                    Column {
+//                        ImageCard(painter = painter, contentDesc = desc, title = title)
+//                    }
+//                }
+//            }
+
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(modifier = Modifier.weight(1f).fillMaxSize()) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDesc: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDesc,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            ) {
+
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorBox(
+    modifier: Modifier,
+    updateColor: (Color) -> Unit
+) {
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestJetpackTheme {
+        val painter = painterResource(id = R.drawable.naruto)
+        val desc = "5555"
+        val title = "Gram"
+
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .padding(16.dp)
+            ) {
+                Column {
+                    ImageCard(painter = painter, contentDesc = desc, title = title)
+                }
+            }
+        }
+    }
+}
